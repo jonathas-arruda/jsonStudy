adf82a55a98daabf9af5489d8d136b2e155d2c60
Jonathas <jonathasarruda@outlook.com>
2022-06-14 12:44:35 -0300

    Start study.

---
+++ js/index.js
@@ -0,0 +1,93 @@
+const jsonData = `
+{
+  "squadName": "Super hero squad",
+  "homeTown": "Metro City",
+  "formed": 2016,
+  "secretBase": "Super tower",
+  "active": true,
+  "members": [
+    {
+      "name": "Molecule Man",
+      "age": 29,
+      "secretIdentity": "Dan Jukes",
+      "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+    ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+    ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+    ]
+    }
+  ]
+}
+`;
+const objData = JSON.parse(jsonData)
+
+//Percorra o JSON e insera os nomes dos membros na lista html
+function percorrerMembros() {
+  //escreva a lógica
+  let membros = document.getElementById("membros")
+
+  
+  objData.members.forEach(member => {
+    let lista=`
+    <ul>
+      <li>Nome: ${member.name}</li>
+      <li>Idade: ${member.age}</li>
+      <li>IdentidadeSecreta: ${member.secretIdentity}</li>
+      <li>Powers:
+        <ul>
+          ${member.powers.map(power =>`<li>${power}</li>`).join("")}       
+        </ul>  
+      </li>
+    </ul>
+    `
+    
+    let novaDiv = document.createElement("div");
+    novaDiv.innerHTML = lista;
+    novaDiv.className ="item";
+    membros.appendChild(novaDiv)
+  });
+
+}
+percorrerMembros();
