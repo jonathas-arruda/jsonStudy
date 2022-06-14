fb66a708f49dbde9523a8dc87690c33af2d47d68
Jonathas <jonathasarruda@outlook.com>
2022-06-14 15:12:20 -0300

    Adding a little bit style.

--- js/index.js
+++ js/index.js
@@ -35,6 +35,22 @@ const jsonData = `
     ]
     },
     {
+      "name": "Molecule Man",
+      "age": 29,
+      "secretIdentity": "Dan Jukes",
+      "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
       "name": "Eternal Flame",
       "age": 1000000,
       "secretIdentity": "Unknown",
@@ -45,6 +61,22 @@ const jsonData = `
         "Teleportation",
         "Interdimensional travel"
     ]
+    },
+    {
+      "name": "Molecule Man",
+      "age": 29,
+      "secretIdentity": "Dan Jukes",
+      "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
     },
     {
       "name": "Eternal Flame",
